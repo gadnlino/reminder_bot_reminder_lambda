0d410044968ae53bd4f87cb4adddd445cbe9a2e4
gadnlino <guiavenas@gmail.com>
2020-04-26 21:52:34 -0300

    Corrigindo erro no envio da mensagem para o sqs

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const awsSvc = require("./services/awsService.js");
-const dotenv = require("dotenv");
+/*const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config();*/
 
 exports.handler = async (event, context) => {
 
     const {REMINDERS_BOT_TABLE, REMINDERS_QUEUE_URL} = process.env;    
 
     return new Promise(async (_, __) => {        
-
-        const {uuid, creation_date, rule_arn, rule_name} = JSON.parse(event);
+        
+        const {uuid, creation_date, rule_arn, rule_name} = event;
 
         const queryResp = await awsSvc.
                 dynamodb.queryItem(REMINDERS_BOT_TABLE, "#uuid = :id",
                                     {"#uuid" : "uuid"}, {":id" : uuid});
         
         const reminder = queryResp.Items[0];
-                
+
         const sendMessageResp = await awsSvc.sqs
             .sendMessage(REMINDERS_QUEUE_URL, JSON.stringify(reminder));
 
