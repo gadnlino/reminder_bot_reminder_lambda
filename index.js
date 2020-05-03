f34ef0495a313a0f5c3e11eed5a22ad12a52b1c7
gadnlino <guiavenas@gmail.com>
2020-05-02 21:15:24 -0300

    Fix: tornando o lambda síncrono

--- index.js
+++ index.js
@@ -3,34 +3,37 @@ const awsSvc = require("./services/awsService.js");
 
 dotenv.config();*/
 
-exports.handler = async (event, context) => {
+exports.handler = (event, context) => {
 
-    const {REMINDERS_BOT_TABLE, REMINDERS_QUEUE_URL} = process.env;    
+    const { REMINDERS_BOT_TABLE, REMINDERS_QUEUE_URL } = process.env;
 
-    return new Promise(async (_, __) => {        
-        
-        const {uuid, creation_date, rule_arn, rule_name} = event;
+    async function remindReminders() {
+        const { uuid, creation_date, rule_arn, rule_name } = event;
+
+        console.log(event);
 
         const queryResp = await awsSvc.
-                dynamodb.queryItem(REMINDERS_BOT_TABLE, "#uuid = :id",
-                                    {"#uuid" : "uuid"}, {":id" : uuid});
-        
+            dynamodb.queryItem(REMINDERS_BOT_TABLE, "#uuid = :id",
+                { "#uuid": "uuid" }, { ":id": uuid });
+
         const reminder = queryResp.Items[0];
 
         const sendMessageResp = await awsSvc.sqs
             .sendMessage(REMINDERS_QUEUE_URL, JSON.stringify(reminder));
 
         const listTargetsResp = await awsSvc.cloudWatchEvents.listTargets(rule_name);
-        
-        const targetIds = listTargetsResp.Targets.map(t=>t.Id);
-        
-        if(targetIds.length > 0){
+
+        const targetIds = listTargetsResp.Targets.map(t => t.Id);
+
+        if (targetIds.length > 0) {
             const removeTargetsResp = await awsSvc
-                            .cloudWatchEvents.removeTargets(targetIds, rule_name);
+                .cloudWatchEvents.removeTargets(targetIds, rule_name);
         }
 
         const deleteRuleResp = await awsSvc.cloudWatchEvents
-                                .deleteRule(rule_name);
-    });
+            .deleteRule(rule_name);
+    }
+
+    remindReminders();
 };
 
